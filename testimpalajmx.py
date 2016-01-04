549a8e1c83ced739a928a5b20451cccd80095f3f
root <root@r2341-d5-us01.dssd.com>
2016-01-04 13:34:54 -0800

    six

--- testimpalajmx.py
+++ testimpalajmx.py
@@ -3,6 +3,8 @@
 import sys
 import urllib2
 import requests
+import json
+
 
 impalad_nodes=['http://r2341-d5-us02:25000','http://r2341-d5-us03:25000','http://r2341-d5-us04:25000']
 jmx_nodes=['http://r2341-d5-us02:50075/jmx?qry=Hadoop:service=DataNode,name=DataNodeActivity-r2341-d5-us02.dssd.com-50010',
@@ -17,8 +19,11 @@ def main():
      print res.text
      
   for hosts in jmx_nodes:
-     res = requests.get(hosts)
-     print res
+     print '-------------------------------------------------------------'
+     req = urllib2.Request(hosts)
+     opener=urllib2.build_opener()
+     stream = opener.open(req)
+     print json.load(stream)
 
 
 if __name__=='__main__':
