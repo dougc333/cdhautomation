3a6b4a41438de7205b618c3c8b575327dd2ea041
dougc333 <dougc@stanfordalumni.org>
2016-01-12 11:38:16 -0800

    fio

--- clean.py
+++ clean.py
@@ -13,6 +13,8 @@ from cm_api.endpoints.cms import ClouderaManager
 from cm_api.endpoints.services import ApiService, ApiServiceSetupInfo
 from time import sleep
 
+import socket
+
 service_types_and_names = {
   "SOLR":"solr",
   "YARN":"yarn",
@@ -23,7 +25,7 @@ service_types_and_names = {
   "IMPALA":"impala"
 }
 
-cm_host="r2341-d5-us31"
+CM_HOST=socket.gethostname()
 cm_port = 7180
 host_list=[
       'r2341-d5-us31.dssd.com',
@@ -47,7 +49,8 @@ def stop_and_remove_services(api):
 
 def cm_args_parser():
   parser = argparse.ArgumentParser()
-  parser.add_argument("--cm-host",default="r2341-d5-us01", help="name of CM host")
+  rint 'using CM_HOST:%s' % CM_HOST
+  parser.add_argument("--cm-host",default=CM_HOST, help="name of CM host")
   parser.add_argument("--cm-user", default="admin", help="name of CM user")
   parser.add_argument("--cm-password", default="admin", help="CM password")
   return parser
@@ -75,13 +78,12 @@ def clean_hdfs(api):
 def main():
   parser = cm_args_parser()
   args = parser.parse_args()
-
   print "Connecting to CM on host " + args.cm_host + "... ",
   api = ApiResource(args.cm_host, 
       username=args.cm_user, password=args.cm_password)
   print "done."
 
-  clean_all(api)
+  #clean_all(api)
 
 
 if __name__ == '__main__':
