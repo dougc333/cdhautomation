2815950e6c2cbb7ab8a892feba38361fdb9701f8
dougc333 <dougc@stanfordalumni.org>
2016-11-15 04:51:19 -0800

    add

---
+++ login.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from cm_api.api_client import ApiResource, ApiException
+
+
+
+def main():
+  """
+  """
+  api = ApiResource("cm_host=","","")
+
+
+if "__name__" == "__main__" :
+  main()
